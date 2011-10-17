d6fb6157b76b6f17a3ddb9388bfe160579cbb3ba
Markus Mayer <development@widemeadows.de>
2011-10-17 14:38:47 +0200

    Tokenbeschreibungen

--- Logic/LanguageParser/TokenDescription.cs
+++ Logic/LanguageParser/TokenDescription.cs
@@ -17,15 +17,29 @@ namespace Logic.LanguageParser
         private readonly Regex _expression;
 
         /// <summary>
+        /// Die Regex
+        /// </summary>
+        public string Expression { get; private set; }
+
+        /// <summary>
+        /// Die Bezeichnung des Tokens
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="TokenDescription"/> class.
         /// </summary>
         /// <param name="regularExpression">The regular expression.</param>
+        /// <param name="description">The description.</param>
         /// <remarks></remarks>
-        public TokenDescription(string regularExpression)
+        public TokenDescription(string regularExpression, string description)
         {
             Contract.Requires(!String.IsNullOrEmpty(regularExpression), "The regular expression must not be empty");
+            Contract.Requires(!String.IsNullOrEmpty(description), "The description must not be empty");
 
+            Expression = regularExpression;
             _expression = new Regex(regularExpression, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+            Description = description;
         }
         
         #region Contracts
--- Logic/Program.cs
+++ Logic/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Logic.LanguageParser;
 
 namespace Logic
 {
@@ -9,6 +10,13 @@ namespace Logic
 	{
 		static void Main(string[] args)
 		{
+            IList<TokenDescription> tokenDescriptions = new List<TokenDescription>();
+            tokenDescriptions.Add(new TokenDescription(@"[\+\*]", "Operatoren (binär)"));
+            tokenDescriptions.Add(new TokenDescription(@"[~']", "Operatoren (unär)"));
+            tokenDescriptions.Add(new TokenDescription(@"[a-zA-Z]+([0-9]*[a-zA-Z]*_*])*", "Bezeichner"));
+            tokenDescriptions.Add(new TokenDescription(@"[()]", "Klammern"));
+
+
 		}
 	}
 }
